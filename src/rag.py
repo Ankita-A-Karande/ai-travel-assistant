e21cac2e2fd35a4154f6d808f114dd4096440a01
Ankita Karande <81807425+Ankita-A-Karande@users.noreply.github.com>
2026-04-07 00:06:42 +0530

    Update rag.py

--- src/rag.py
+++ src/rag.py
@@ -1,6 +1,6 @@
 # services/rag.py
 
-from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitters import CharacterTextSplitter
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
